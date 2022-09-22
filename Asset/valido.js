da5f38da005b08ccf6b467e52832e54d3889266b
rolando90fsj <rolando.fdezs@outlook.com>
2022-09-22 19:17:11 -0400

    Update valido.js
    actual valido.js

--- Asset/valido.js
+++ Asset/valido.js
@@ -21,7 +21,7 @@ function validarRango(elemento){
 
     //  var numero = prompt("¿Qué tabla quieres imprimir?");
 
-      for(var i = 1; i < 11; i++ ) { //el ciclo va a parar cuando sea 11 porque necesitamos imprimir 10 lineas que son las 10 multiplicaciones de la tabla - con el 1++ le decimos al ciclo que cada vez que itere lo incremente
+      for(var i = 1; i < numero; i++ ) { //el ciclo va a parar cuando sea 11 porque necesitamos imprimir 10 lineas que son las 10 multiplicaciones de la tabla - con el 1++ le decimos al ciclo que cada vez que itere lo incremente
          document.write (numero + " x " + i + " = " + (numero* i ) + "<br>"); // aquí se escribe cada línea aparte, una detrás de otra - tomamos el valor de tabla  y lo * por i 
           //con el <br> le estamos diciendo al html que vamos a saltar de línea 
       }
